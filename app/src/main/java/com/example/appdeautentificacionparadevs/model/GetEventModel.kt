161f421e2fdf853cbde97560a0538f98e0de9c84
cor22982 <cor22982@uvg.edu.gt>
2023-11-19 04:26:09 -0600

    Mi app

---
+++ app/src/main/java/com/example/appdeautentificacionparadevs/model/GetEventModel.kt
@@ -0,0 +1,8 @@
+package com.example.appdeautentificacionparadevs.model
+
+data class GetEventModel(
+    var id: Int = 0,
+    var summary: String? = "",
+    var startDate: String = "",
+    var endDate:String =""
+)
